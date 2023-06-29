5f1a87cb161ba80dd2e977d40a8acd74cb968e02
Aaveg-Tomar <tomaraaveg@gmail.com>
2023-06-29 11:18:16 +0530

    change value using function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-//      Student kunal = new Student("Aaveg" , 1 , 90 );
-        Student[] std = new Student[5];
+      Student kunal = new Student("Aaveg" , 1 , 90 );
+//        Student[] std = new Student[5];
 
-        for (int i = 0; i < 5; i++) {
-            String x = in.next();
-            int y = in.nextInt();
-            int z = in.nextInt();
+//        Student kunal = new Student();
 
-           Student a =  new Student(x , y , z);
-            System.out.println(a.name + a.roll + a.marks);
+        // call the change function
+        kunal.changename("Aayush");
 
-        }
+        kunal.greeting();
+
+//        for (int i = 0; i < 5; i++) {
+//            String x = in.next();
+//            int y = in.nextInt();
+//            int z = in.nextInt();
+//
+//           Student a =  new Student(x , y , z);
+//            System.out.println(a.name + a.roll + a.marks);
+//
+//        }
+
+
 
 
 
@@ -32,6 +41,22 @@ class  Student{
     int roll;
     int marks;
 
+    public Student() {
+
+    }
+
+    // change the value of name
+    public  void changename(String newname)
+    {
+        name = newname;
+    }
+
+
+    public void greeting()
+    {
+        System.out.println("Hello World  " + name);
+    }
+
 
     Student(String name , int roll , int marks )
     {
@@ -39,5 +64,7 @@ class  Student{
         this.roll = roll;
         this.marks = marks;
     }
+
+
 
 }
