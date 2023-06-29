6c28b1dc5b67a57866a2ab202ade342d0f57c497
Aaveg-Tomar <tomaraaveg@gmail.com>
2023-06-29 10:56:06 +0530

    create class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//      Student kunal = new Student("Aaveg" , 1 , 90 );
+        Student[] std = new Student[5];
+
+        for (int i = 0; i < 5; i++) {
+            String x = in.next();
+            int y = in.nextInt();
+            int z = in.nextInt();
+
+           Student a =  new Student(x , y , z);
+            System.out.println(a.name + a.roll + a.marks);
+
+        }
+
+
+
 
     }
+}
+
+// Create a class
+
+class  Student{
+    String name;
+    int roll;
+    int marks;
+
+
+    Student(String name , int roll , int marks )
+    {
+        this.name = name;
+        this.roll = roll;
+        this.marks = marks;
+    }
+
 }
