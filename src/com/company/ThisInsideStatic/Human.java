cf3e7395e00d68407d0411aa7300bc999b2cb95b
Aaveg-Tomar <tomaraaveg@gmail.com>
2023-07-02 10:10:31 +0530

    calling static and non-static function of different class

--- src/com/company/ThisInsideStatic/Human.java
+++ src/com/company/ThisInsideStatic/Human.java
@@ -1,4 +1,30 @@
 package com.company.ThisInsideStatic;
 
 public class Human {
+
+    int age;
+    String name;
+    boolean married;
+
+    static  void  abc()
+    {
+        Human hm1 = new Human();
+        hm1.xyz();
+        System.out.println("hello");
+//        System.out.println(this.age);  cannot use this inside the static
+    }
+
+    void xyz()
+    {
+        System.out.println("Humans");
+    }
+
+    Human(){}
+
+     public Human(int age ,String name , boolean married)
+    {
+        this.age = age;
+        this.name = name;
+        this.married = married;
+    }
 }
--- src/com/company/ThisInsideStatic/Main.java
+++ src/com/company/ThisInsideStatic/Main.java
@@ -1,4 +1,20 @@
 package com.company.ThisInsideStatic;
 
 public class Main {
+    public static void main(String[] args) {
+        Human x = new Human(23 , "kunal" , true);
+        Human y = new Human(36  , "g" , false);
+
+        // abc is static function
+        Human.abc();
+
+
+        // calling non-static function
+        Human z = new Human();
+        z.xyz();
+
+
+
+
+    }
 }
