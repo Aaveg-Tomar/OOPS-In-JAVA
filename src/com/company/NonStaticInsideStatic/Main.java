a94a20eadf587efd7f655b8952982123e26c4b1d
Aaveg-Tomar <tomaraaveg@gmail.com>
2023-07-02 09:51:15 +0530

    Non-Static Inside the Static

---
+++ src/com/company/NonStaticInsideStatic/Main.java
@@ -0,0 +1,29 @@
+package com.company.NonStaticInsideStatic;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Main mn = new Main();
+        mn.greeting();
+        
+    }
+
+    // fun is not dependent on the object
+    static  void fun()
+    {
+       //  greeting();  // you can't use because it requires an instance
+        // but the function you are using it in does not depend on instance.
+
+        // You can not access non static stuff without referencing their in
+        // a static context.
+        Main mn1 = new Main();
+        mn1.greeting();
+    }
+    
+    // we know that something which is not static , belong to an object
+    void greeting()
+    {
+       //  fun();
+        System.out.println("hello world");
+    }
+}
--- src/com/company/staticExample/Main.java
+++ src/com/company/staticExample/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Human hm1 = new Human(22 , "Kunal" , 10000 , false);
         Human hm2 = new Human(24 , "Rahul" , 20000 , true);
+        Human hm3 = new Human(25 , "arpit" , 3000 , true);
 
-        System.out.println(hm1.population);
+        System.out.println(Human.population);
         System.out.println(hm2.population);
+        System.out.println(Human.population);
 
     }
 }
